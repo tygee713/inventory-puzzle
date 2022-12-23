da001280c123bd7d83902f67d3f3bf6aa3bd003a
Tyler Geerdts <tygee713@gmail.com>
2022-12-23 13:50:45 -0500

    Finished most of main scene functions

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -14,27 +14,36 @@ const ProficiencyMeter = Bar({})
 const GoldMeter = Bar({})
 const WeightMeter = Bar({})
 
-export default Scene({
+const sections = {
+  0: Drops,
+  1: Inventory,
+  2: Discard,
+}
+
+const scene = Scene({
   id: 'main',
   level: 1,
+  hoveredSection: null,
+  selectedSection: null,
   selectedItemId: null,
   movingItemId: null,
   itemMenuOpen: false,
-  objects: [Drops, Inventory, Discard, ProficiencyMeter, GoldMeter, WeightMeter],
+  objects: [Cursor, Drops, Inventory, Discard, ProficiencyMeter, GoldMeter, WeightMeter],
   createBossItems: function() {
     // create x items of y rarity, depending on level
+    let bossItems = []
+    // pull from a list of items that can appear in a level
+    // get a variety of different item types, rarities
     // set those items as children of the Drops object
+    Drops.items.push(bossItems)
   },
   createStartingInventory: function() {
     // create x items of y rarity, depending on level
+    let startingItems = []
+    // pull from a list of items that can appear in a level
+    // get a variety of different item types, rarities
     // set those items as children of the Inventory object
-  },
-  moveCursor: function() {
-    // move the cursor to the next available cell in the current grid
-    // if one does not exist, move to the next grid
-    // if no next grid, do not move
-    // cannot move cursor at all if an item menu is open
-    // if an item is selected, move item icon along with cursor
+    Inventory.items.push(startingItems)
   },
   placeItem: function(cellId) {
     // if current space is occupied, replace item with currently moving item
@@ -63,7 +72,7 @@ export default Scene({
     // unselect the item
     this.selectedItemId = null
     // move item from its current location to the discard area
-    Discard.add(itemId)
+    Discard.items.push(itemId)
   },
   startMoveItem: function(itemId) {
     // change the moving item
@@ -73,3 +82,129 @@ export default Scene({
     // after each time the items in inventory are changed, calculate new meter values
   }
 })
+
+// TODO: support A button on gamepad
+onKey('z', () => {
+  // if an item menu is open, select the hovered option
+
+  // otherwise, if a section is selected, select the hovered item
+
+  // otherwise, select the hovered section
+})
+// TODO: support B button on gamepad
+onKey('x', () => {
+  // if an item is being moved, put it back in its origin
+
+  // otherwise, if an item menu is open, close the item menu
+
+  // otherwise, unselect a hovered section
+})
+
+// TODO: support gamepad directionals
+
+onKey('up', () => {
+  // if an item menu is open, change the selected option
+  if (scene.itemMenuOpen) {
+    Item[selectedItemId].itemMenu.up()
+    // otherwise, if there is a selected section, change the position of the cursor relative to that section
+  } else if (scene.selectedSection) {
+    let section = sections[scene.selectedSection]
+    let newCell = section.upFrom(Cursor.position)
+    Cursor.position = newCell.position
+    Cursor.x = newCell.x
+    Cursor.y = newCell.y
+    // otherwise, cycle through the sections
+  } else {
+    if (scene.hoveredSection = 0) {
+      scene.hoveredSection = 2
+    } else {
+      scene.hoveredSection -= 1
+    }
+    Cursor.position = scene.hoveredSection
+    let section = sections[scene.hoveredSection]
+    Cursor.x = section.x
+    Cursor.y = section.y
+    Cursor.width = section.width
+    Cursor.height = section.height
+  }
+})
+onKey('down', () => {
+  // if an item menu is open, change the selected option
+  if (scene.itemMenuOpen) {
+    Item[selectedItemId].itemMenu.down()
+    // otherwise, if there is a selected section, change the position of the cursor relative to that section
+  } else if (scene.selectedSection) {
+    let section = sections[scene.selectedSection]
+    let newCell = section.downFrom(Cursor.position)
+    Cursor.position = newCell.position
+    Cursor.x = newCell.x
+    Cursor.y = newCell.y
+    // otherwise, cycle through the sections
+  } else {
+    if (scene.hoveredSection = 0) {
+      scene.hoveredSection = 2
+    } else {
+      scene.hoveredSection -= 1
+    }
+    Cursor.position = scene.hoveredSection
+    let section = sections[scene.hoveredSection]
+    Cursor.x = section.x
+    Cursor.y = section.y
+    Cursor.width = section.width
+    Cursor.height = section.height
+  }
+})
+onKey('left', () => {
+  // if an item menu is open, change the selected option
+  if (scene.itemMenuOpen) {
+    Item[selectedItemId].itemMenu.left()
+    // otherwise, if there is a selected section, change the position of the cursor relative to that section
+  } else if (scene.selectedSection) {
+    let section = sections[scene.selectedSection]
+    let newCell = section.leftFrom(Cursor.position)
+    Cursor.position = newCell.position
+    Cursor.x = newCell.x
+    Cursor.y = newCell.y
+    // otherwise, cycle through the sections
+  } else {
+    if (scene.hoveredSection = 0) {
+      scene.hoveredSection = 2
+    } else {
+      scene.hoveredSection -= 1
+    }
+    Cursor.position = scene.hoveredSection
+    let section = sections[scene.hoveredSection]
+    Cursor.x = section.x
+    Cursor.y = section.y
+    Cursor.width = section.width
+    Cursor.height = section.height
+  }
+})
+onKey('right', () => {
+  // if an item menu is open, change the selected option
+  if (scene.itemMenuOpen) {
+    Item[selectedItemId].itemMenu.right()
+    // otherwise, if there is a selected section, change the position of the cursor relative to that section
+  } else if (scene.selectedSection) {
+    let section = sections[scene.selectedSection]
+    let newCell = section.rightFrom(Cursor.position)
+    Cursor.position = newCell.position
+    Cursor.x = newCell.x
+    Cursor.y = newCell.y
+    // otherwise, cycle through the sections
+  } else {
+    if (scene.hoveredSection = 0) {
+      scene.hoveredSection = 2
+    } else {
+      scene.hoveredSection -= 1
+    }
+    Cursor.position = scene.hoveredSection
+    let section = sections[scene.hoveredSection]
+    Cursor.x = section.x
+    Cursor.y = section.y
+    Cursor.width = section.width
+    Cursor.height = section.height
+  }
+})
+
+export default scene
